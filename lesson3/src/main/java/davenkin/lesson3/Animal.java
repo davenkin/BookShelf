619564932df6a7c732fede4692e54aa48fb78df5
davenkin <davenkin@gmail.com>
2014-07-30 11:58:26 +0530

    lesson3:walk() on Walkable interface

--- lesson3/src/main/java/davenkin/lesson3/Animal.java
+++ lesson3/src/main/java/davenkin/lesson3/Animal.java
@@ -3,12 +3,11 @@ package davenkin.lesson3;
 /**
  * Created by Davenkin on 7/30/14.
  */
-public abstract class Animal {
+public abstract class Animal implements Walkable {
     protected int weight;
 
     public void setWeight(int weight) {
         this.weight = weight;
     }
 
-    public abstract void walk();
 }
---
+++ lesson3/src/main/java/davenkin/lesson3/Walkable.java
@@ -0,0 +1,8 @@
+package davenkin.lesson3;
+
+/**
+ * Created by Davenkin on 7/30/14.
+ */
+public interface Walkable {
+    void walk();
+}
