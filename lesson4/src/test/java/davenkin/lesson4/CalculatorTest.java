37e8b7b4122604a394b953aa32461b885c780966
davenkin <davenkin@gmail.com>
2014-08-03 18:21:10 +0530

    lesson4: added validation logic for calculator

--- lesson4/src/test/java/davenkin/lesson4/CalculatorTest.java
+++ lesson4/src/test/java/davenkin/lesson4/CalculatorTest.java
@@ -1,23 +1,43 @@
 package davenkin.lesson4;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
 public class CalculatorTest {
 
+    private Calculator calculator;
+
+    @Before
+    public void setUp() throws Exception {
+        calculator = new Calculator();
+    }
+
     @Test
     public void shouldAddNumbersMathematically() {
-        Calculator calculator = new Calculator();
         String result = calculator.add("12", "23");
         assertEquals("35", result);
     }
 
     @Test
-    public void shouldAddNumbersConcatly() {
-        Calculator calculator = new Calculator();
+    public void shouldConcatNumbers() {
         String result = calculator.concat("12", "23");
         assertEquals("1223", result);
+    }
+
+
+    @Test
+    public void shouldAddNumbersBetween1And100() {
+        String result = calculator.add("101", "23");
+        assertEquals("ERROR", result);
+    }
+
+
+    @Test
+    public void shouldConcatNumbersBetween1And100() {
+        String result = calculator.concat("101", "23");
+        assertEquals("ERROR", result);
     }
 
 }
