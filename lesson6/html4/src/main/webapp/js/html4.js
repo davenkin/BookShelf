4276f74d4a4f66f85c542ab78e71652fa35fd807
davenkin <davenkin@gmail.com>
2014-09-17 00:31:07 +0530

    lesson6:added responsive css, hide left bar for mobile

--- lesson6/html4/src/main/webapp/js/html4.js
+++ lesson6/html4/src/main/webapp/js/html4.js
@@ -2,5 +2,13 @@ window.onload=relocateMainContent;
 
 function relocateMainContent() {
     var topHeight = document.getElementById("top-wrapper").clientHeight;
-    document.getElementById("main-content").style.marginTop=topHeight+"px";
+    var footerHeight = document.getElementById("footer").clientHeight;
+    var windowHeight = window.innerHeight
+
+//    var windowWidth = window.innerWidth
+//    document.getElementById("main-content").innerHTML = windowHeight +"+"+windowWidth+"+"+topHeight +"+"+footerHeight +"px";
+
+    document.getElementById("main-content-wrapper").style.paddingTop=topHeight+"px";
+    document.getElementById("main-content").style.height=(windowHeight-topHeight)+"px";
 }
+
