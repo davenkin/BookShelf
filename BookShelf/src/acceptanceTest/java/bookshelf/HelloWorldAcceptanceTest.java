6d1c385ec87eefe10bbd8573eb15169b43473f60
davenkin <davenkin@gmail.com>
2014-10-04 16:39:37 +0530

    lesson8:added comment for firefox

--- BookShelf/src/acceptanceTest/java/bookshelf/HelloWorldAcceptanceTest.java
+++ BookShelf/src/acceptanceTest/java/bookshelf/HelloWorldAcceptanceTest.java
@@ -2,6 +2,7 @@ package bookshelf;
 
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 
 import static org.junit.Assert.assertTrue;
@@ -14,6 +15,7 @@ public class HelloWorldAcceptanceTest {
     @Test
     public void testHelloWorld() {
 
+        //can change to FirefoxDriver if you want to see the browser
         WebDriver driver = new HtmlUnitDriver();
 
         driver.get("http://localhost:8080/BookShelf/helloworld");
